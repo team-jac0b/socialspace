10eb01aa3c388e1ea842b19ee7edc7a6bd991c89
Jacob Elias <jacobeelias@gmail.com>
2019-05-20 16:01:06 -0700

    built face recognition counter
    also added test images

---
+++ facial-recognition.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import face_recognition
+
+# Load the jpg file into a numpy array
+image = face_recognition.load_image_file("group-6.jpg")
+
+# Find all the faces in the image using the default HOG-based model.
+# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
+# See also: find_faces_in_picture_cnn.py
+face_locations = face_recognition.face_locations(image)
+
+print("{}".format(len(face_locations)))
