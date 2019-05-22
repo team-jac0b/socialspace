3f2d8910725757007012e806c5e5d9484191286c
Jacob Elias <jacobeelias@gmail.com>
2019-05-22 16:26:10 -0700

    added new spacy code

---
+++ jacob-tries-spacy.py
@@ -0,0 +1,25 @@
+# spacy.explain('<tag>')
+
+#
+# import spacy
+# nlp = spacy.load("en_core_web_sm")
+# doc = nlp(u"This is a sentence.")
+# print([(w.text, w.pos_) for w in doc])
+
+import spacy
+# spacy.explain('pos)
+
+nlp = spacy.load("en_core_web_sm")
+
+text = "It’s official: Apple is the first U.S. public company to reach a $1 trillion market value"
+
+# Process the text
+doc = nlp(text)
+
+for token in doc:
+    # Get the token text, part-of-speech tag and dependency label
+    token_text = token.text
+    token_pos = token.pos_
+    token_dep = token.dep_
+    # This is for formatting only
+    print("{:<12}{:<10}{:<10}".format(token_text, token_pos, token_dep))
